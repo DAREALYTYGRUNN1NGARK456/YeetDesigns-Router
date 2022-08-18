54550ff3181ca584a93acc23c7392c5c72995695
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2022-08-18 11:55:33 -0400

    Update TestIndex.js

--- Test/TestIndex.js
+++ Test/TestIndex.js
@@ -1,5 +1,20 @@
-const Utils = require("../src/utils")
+const {Utils} = require("../")
 const express = require("express")
-const app = express()
-const mdir = __dirname
-const util = new Utils(app, "ejs", "YeetDesigns", mdir+"/pages")
+const site = express()
+
+const server = new Utils({
+    app: site, // express application
+    type: "ejs", // render middleware
+    renderFile: "html", // render page file extenison
+    pagesPath: __dirname+"/pages", // pages path
+    assets: {
+        assetsFolderPath: __dirname+"/assets", // loads the assets folder which contains css/Main.css and scripts/Main.js
+        assetsPath: "/assets" // the url path
+    }  
+})
+
+server.load(__dirname+"/routes", function () {
+    site.listen(8080, () => {
+        console.log("Site is online at https://localhost:8080")
+    })
+})
